557699b10678bc7689ed482a0bd4d62a49614bf8
BSCoding0 <bscoding0@gmail.com>
2022-11-01 00:52:12 +0100

    LOL gone

--- src/main/java/net/bsanimation/examplemod/ExampleMod.java
+++ src/main/java/net/bsanimation/examplemod/ExampleMod.java
@@ -7,7 +7,7 @@ import org.slf4j.LoggerFactory;
 public class ExampleMod implements ModInitializer {
 	public static final String MOD_ID = "examplemod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-	//lol
+
 	@Override
 	public void onInitialize() {
 	}
