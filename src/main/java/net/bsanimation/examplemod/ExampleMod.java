8f66d328d503ea187e37159e72d8f8e005985d23
BSCoding0 <bscoding0@gmail.com>
2022-11-01 00:51:18 +0100

    LOL

---
+++ src/main/java/net/bsanimation/examplemod/ExampleMod.java
@@ -0,0 +1,14 @@
+package net.bsanimation.examplemod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ExampleMod implements ModInitializer {
+	public static final String MOD_ID = "examplemod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	//lol
+	@Override
+	public void onInitialize() {
+	}
+}
